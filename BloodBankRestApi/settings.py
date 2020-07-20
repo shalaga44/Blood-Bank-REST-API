53c1f9e941fbef1f539e94e82755d493c2fc432e
Mohamed Moawia <mohamedmoawia4@gmail.com>
2020-07-20 23:44:53 +0200

    ENABLE gzip Functionality

--- BloodBankRestApi/settings.py
+++ BloodBankRestApi/settings.py
@@ -137,3 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
