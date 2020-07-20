196af3884292df7e2778276aee8697aa935a28c5
Mohamed Moawia <mohamedmoawia4@gmail.com>
2020-07-20 23:21:07 +0200

    ACTIVATE Django-Heroku (ready to deploy)

--- BloodBankRestApi/settings.py
+++ BloodBankRestApi/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
